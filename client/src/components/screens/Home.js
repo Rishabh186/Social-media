3424e9d995267650b5c28aada4410bdbd7e9121a
Rishabh186 <yrishabh94@gmail.com>
2020-08-11 15:51:35 +0530

    client side

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -18,7 +18,7 @@ const Home=()=>{
          </div>
 
          <div className="card home-card">
-           <h4>Lucky</h4>
+           <h4>Lucky</h4> 
           <div className="card-image">
                  <img src="https://images.unsplash.com/photo-1558981285-6f0c94958bb6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"/>
              </div>
